d5f8814770b3c684df646422aeb7971012ab9053
kuriki <KyrelJerome@Users.noreply.github.com>
2022-02-18 19:34:47 -0500

    Add requirements.txt

--- week1/main.py
+++ week1/main.py
@@ -1,5 +1,12 @@
 import gym
+from time import sleep
 from matplotlib import pyplot as plt
 env = gym.make("CartPole-v1")
 observation = env.reset()
 
+for _ in range(1000):
+    sleep(0.1)
+    env.render()
+    print(env.action_space.sample())
+    env.step(env.action_space.sample()) # take a random action
+env.close()
